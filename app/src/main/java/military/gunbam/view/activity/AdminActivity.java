759af1ba9879bc8d3a71488f16288fb7861b8d79
Song Ye Bin <75508349+enddl3788@users.noreply.github.com>
2023-10-10 13:22:03 +0900

    feat: 관리자 페이지 테스트 게시물 추가 개선 # 테스트 게시물 수 변경 10 -> 25 # 각 게시판마다 5개씩 생성됨

--- app/src/main/java/military/gunbam/view/activity/AdminActivity.java
+++ app/src/main/java/military/gunbam/view/activity/AdminActivity.java
@@ -52,60 +52,63 @@ public class AdminActivity extends BasicActivity {
                 case R.id.admin_menu_1_button:
                     if (user != null) {
                         String userUid = user.getUid();
-                        // 테스트 게시물 10개를 작성합니다.
-                        for (int i = 0; i < 10; i++) {
-                            // 게시물 데이터 생성
-                            String title = "테스트 게시물 " + (i + 1);
-                            Date date = new Date();
-                            int recommendationCount = i;
-                            Boolean isAnonymous;
-                            if (i % 2 == 1) {
-                                isAnonymous = true;
-                            } else {
-                                isAnonymous = false;
+
+                        String[] boardNames = {"자유게시판", "정보게시판", "군인게시판", "신병게시판", "비밀게시판"};
+                        for (String boardName : boardNames) {
+                            // 테스트 게시물 5개씩 작성합니다.
+                            for (int i = 0; i < 5; i++) {
+                                // 게시물 데이터 생성
+                                String title = "테스트 게시물 " + (i + 1);
+                                Date date = new Date();
+                                int recommendationCount = i;
+                                Boolean isAnonymous;
+                                if (i % 2 == 1) {
+                                    isAnonymous = true;
+                                } else {
+                                    isAnonymous = false;
+                                }
+
+                                // contentsList와 formatList를 생성
+                                ArrayList<String> contentsList = new ArrayList<>();
+                                ArrayList<String> formatList = new ArrayList<>();
+
+                                // 텍스트 추가
+                                String textContent = "이것은 텍스트 내용입니다.";
+                                contentsList.add(textContent);
+                                formatList.add("text");
+
+                                if (i % 5 == 0) {
+                                    // 이미지 추가
+                                    String imageUrl = "http://luxblock.co.kr/file_data/luxblook/2020/08/17/4b0708ca352f2f903ed0ef0162bac4f2.png"; // 이미지 URL 예시
+                                    contentsList.add(imageUrl);
+                                    formatList.add("image");
+                                }
+
+                                // 게시물 데이터를 Map으로 매핑
+                                Map<String, Object> postMap = new HashMap<>();
+                                postMap.put("title", title);
+                                postMap.put("contents", contentsList);
+                                postMap.put("formats", formatList);
+                                postMap.put("publisher", userUid);
+                                postMap.put("createdAt", date);
+                                postMap.put("isAnonymous", isAnonymous);
+                                postMap.put("recommendationCount", recommendationCount);
+                                postMap.put("boardName", boardName);
+
+                                // 게시물 Firestore에 추가
+                                firestore.collection("posts")
+                                        .add(postMap)
+                                        .addOnSuccessListener(documentReference -> {
+                                            showToast(AdminActivity.this, "게시물이 추가되었습니다");
+                                        })
+                                        .addOnFailureListener(e -> {
+                                            showToast(AdminActivity.this, "게시물 추가 실패");
+                                        });
+
+                                // ArrayList 초기화
+                                contentsList.clear();
+                                formatList.clear();
                             }
-                            String boardName = "테스트";
-
-                            // contentsList와 formatList를 생성
-                            ArrayList<String> contentsList = new ArrayList<>();
-                            ArrayList<String> formatList = new ArrayList<>();
-
-                            // 텍스트 추가
-                            String textContent = "이것은 텍스트 내용입니다.";
-                            contentsList.add(textContent);
-                            formatList.add("text");
-
-                            if (i % 5 == 0) {
-                                // 이미지 추가
-                                String imageUrl = "http://luxblock.co.kr/file_data/luxblook/2020/08/17/4b0708ca352f2f903ed0ef0162bac4f2.png"; // 이미지 URL 예시
-                                contentsList.add(imageUrl);
-                                formatList.add("image");
-                            }
-
-                            // 게시물 데이터를 Map으로 매핑
-                            Map<String, Object> postMap = new HashMap<>();
-                            postMap.put("title", title);
-                            postMap.put("contents", contentsList);
-                            postMap.put("formats", formatList);
-                            postMap.put("publisher", userUid);
-                            postMap.put("createdAt", date);
-                            postMap.put("isAnonymous", isAnonymous);
-                            postMap.put("recommendationCount", recommendationCount);
-                            postMap.put("boardName", boardName);
-
-                            // 게시물 Firestore에 추가
-                            firestore.collection("posts")
-                                    .add(postMap)
-                                    .addOnSuccessListener(documentReference -> {
-                                        showToast(AdminActivity.this, "게시물이 추가되었습니다");
-                                    })
-                                    .addOnFailureListener(e -> {
-                                        showToast(AdminActivity.this, "게시물 추가 실패");
-                                    });
-
-                            // ArrayList 초기화
-                            contentsList.clear();
-                            formatList.clear();
                         }
                     } else {
                         showToast(AdminActivity.this, "사용자가 로그인되어 있지 않습니다.");
